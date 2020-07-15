d6cc4c4c4841d9be05d2e4531c6785084aa9e857
Nathan Thomas <nathan.allan.thomas@gmail.com>
2020-07-15 01:17:10 +0100

    Unit 1 basics completed

---
+++ Prototype 1/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20;
+    public float turnSpeed = 10;
+    public float horizontalInput;
+    public float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Move the vehicle forward on a vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
